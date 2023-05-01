4f04380aa500375b6ba4823e2fb275e506c48136
Felipe Cipriano <feciprianoferreira@gmail.com>
2023-05-01 09:39:46 -0300

    Resolvido e acrescentado recurso visual de mudar a cor dos links no nav de acordo com a section em que o usuário está na página, utilzando Html e Javascript

--- script.js
+++ script.js
@@ -11,7 +11,20 @@ window.onscroll = () => {
         if(top >= offset && top < offset + height){
             navLinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector('header nav a [href*=' + id + ']').classList.add('active');
+                /*
+                    O código aqui está usando o comando 'document.querySelector' para pesquisar
+                    no html usando este script pelo que foi especificado, nesse caso o elemento 'a'
+                    no 'nav' em 'header' no body que contenha a propriedade href sendo igual a variável 'id'
+                    criada anteriormente ('header nav a[href*=' + id + ']'), para após encontrá-lo usar a sintaxe 
+                    '.classList.add('active')' e adicionar o termo 'active' a classe do elemento pesquisado.
+
+                    Um erro que obti ao procurar implementar esse comando foi o de tentar usar o
+                    '.classList.add' em um elemento vazio. Isso se deu pois na sintaxe inseri
+                    'header nav a [href*=' + id + ']' com um espaço entre 'a' e a o detalhamento do
+                    valor da variável id, fazendo assim com que o compilador não encontre e em vez disso
+                    retorne um erro de inserir uma classe num elemento nulo.
+                */
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
         };
     })
